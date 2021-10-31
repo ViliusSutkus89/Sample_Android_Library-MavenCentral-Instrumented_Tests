9945a55f91c31ae65b04714678a62cbf9b1ae8c7
Vilius Sutkus '89 <ViliusSutkus89@gmail.com>
2021-10-31 16:42:43 +0200

    [Library] Expose getVersionName and getVersionCode

--- lib/src/main/java/com/viliussutkus89/samplelib/VersionGetter.java
+++ lib/src/main/java/com/viliussutkus89/samplelib/VersionGetter.java
@@ -5,6 +5,14 @@ public class VersionGetter {
         return "Code=" + BuildConfig.VERSION_CODE + ", name=" + BuildConfig.VERSION_NAME;
     }
 
+    public static String getVersionName() {
+        return BuildConfig.VERSION_NAME;
+    }
+
+    public static long getVersionCode() {
+        return BuildConfig.VERSION_CODE;
+    }
+
     public static String getGitCommit() {
         return BuildConfig.GIT_COMMMIT;
     }
