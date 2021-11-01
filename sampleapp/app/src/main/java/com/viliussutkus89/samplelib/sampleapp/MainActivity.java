6bb1a16d2bc0a6c5751fb0005947e26096b70fd6
Vilius Sutkus '89 <ViliusSutkus89@gmail.com>
2021-11-01 14:17:42 +0200

    [AppOnly] Remove AppCompat dependency
    Use regular Activity and regular theme instead

--- sampleapp/app/src/main/java/com/viliussutkus89/samplelib/sampleapp/MainActivity.java
+++ sampleapp/app/src/main/java/com/viliussutkus89/samplelib/sampleapp/MainActivity.java
@@ -1,11 +1,11 @@
 package com.viliussutkus89.samplelib.sampleapp;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
-import androidx.appcompat.app.AppCompatActivity;
 import com.viliussutkus89.samplelib.VersionGetter;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
