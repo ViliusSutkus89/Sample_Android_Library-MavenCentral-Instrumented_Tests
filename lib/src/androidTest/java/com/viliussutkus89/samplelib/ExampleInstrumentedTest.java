6187ecfcd1b7b9db1e6cb6071c135f28012cd68e
Vilius Sutkus '89 <ViliusSutkus89@gmail.com>
2022-02-06 01:23:56 +0200

    Attempt at working around storage permission in tests

--- lib/src/androidTest/java/com/viliussutkus89/samplelib/ExampleInstrumentedTest.java
+++ lib/src/androidTest/java/com/viliussutkus89/samplelib/ExampleInstrumentedTest.java
@@ -4,12 +4,14 @@ import static org.junit.Assert.assertNotEquals;
 
 import android.Manifest;
 import android.graphics.Bitmap;
+import android.os.Build;
 
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.GrantPermissionRule;
 import androidx.test.runner.screenshot.ScreenCapture;
 import androidx.test.runner.screenshot.Screenshot;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.RuleChain;
@@ -26,23 +28,32 @@ import java.io.IOException;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
     @Rule
-    public RuleChain screenshotRule = RuleChain
-            .outerRule(GrantPermissionRule.grant(Manifest.permission.WRITE_EXTERNAL_STORAGE))
-            .around(new TestWatcher() {
-                @Override
-                protected void failed(Throwable e, Description description) {
-                    super.failed(e, description);
-                    ScreenCapture capture = Screenshot.capture()
-                            .setName(description.getTestClass().getSimpleName() + "-" + description.getMethodName())
-                            .setFormat(Bitmap.CompressFormat.PNG);
-                    try {
-                        capture.process();
-                    } catch (IOException err) {
-                        err.printStackTrace();
-                    }
-                }
-            });
+    public RuleChain screenshotRule;
+
+    @Before
+    public void screenshotRuleChain() {
+        // Android R requires storage permission finessing
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.R) {
+            screenshotRule = RuleChain
+                    .outerRule(GrantPermissionRule.grant(Manifest.permission.WRITE_EXTERNAL_STORAGE))
+                    .around(new TestWatcher() {
+                        @Override
+                        protected void failed(Throwable e, Description description) {
+                            super.failed(e, description);
+                            ScreenCapture capture = Screenshot.capture()
+                                    .setName(description.getTestClass().getSimpleName() + "-" + description.getMethodName())
+                                    .setFormat(Bitmap.CompressFormat.PNG);
+                            try {
+                                capture.process();
+                            } catch (IOException err) {
+                                err.printStackTrace();
+                            }
+                        }
+                    });
+        }
+    }
 
     @Test
     public void useGetVersion() {
